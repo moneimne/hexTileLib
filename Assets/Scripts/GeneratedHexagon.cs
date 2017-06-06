b0d11f267a9b62e51af629eb22fa71f9f6486bd9
mccannd <mccannd@seas.upenn.edu>
2017-06-05 22:40:53 -0400

    adding preliminary hex work

---
+++ Assets/Scripts/GeneratedHexagon.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneratedHexagon : MonoBehaviour {
+
+	public Material meshMat;
+	private static Mesh mesh;
+
+	void Start () {
+		gameObject.AddComponent<MeshFilter>();
+		gameObject.AddComponent<MeshRenderer>();
+		//Vector3 p = gameObject.GetComponent<Transform> ().position;
+
+		if (mesh != null) {
+			gameObject.GetComponent<MeshFilter> ().mesh = mesh;
+			gameObject.GetComponent<MeshRenderer> ().material = meshMat;
+			return;
+		} else 
+			mesh = GetComponent<MeshFilter>().mesh;
+
+
+		mesh.Clear();
+
+		List<Vector3> vertices = new List<Vector3> ();
+		List<Vector3> normals = new List<Vector3> ();
+		List<int> triangles = new List<int> ();
+
+
+		//hexagon top
+		Vector3 up = new Vector3(0, 1, 0);
+		vertices.Add(new Vector3(0, 0, 0));
+		normals.Add (new Vector3(up.x, up.y, up.z));
+
+		for (int i = 0; i < 6; i++) {
+			vertices.Add(new Vector3(Mathf.Sin(Mathf.PI / 3.0f * (i + 0.5f)), 0.0f, Mathf.Cos(Mathf.PI / 3.0f * (i + 0.5f))));
+			normals.Add(new Vector3(up.x, up.y, up.z));
+			int l = i + 1;
+			int r = i + 2;
+			r = r > 6 ? 1 : r;
+			triangles.Add (0);
+			triangles.Add (l);
+			triangles.Add (r);
+		}
+
+		int sideIdx = 7;
+
+		// hexagon sides
+		for (int i = 0; i < 6; i++) {
+			vertices.Add(new Vector3(Mathf.Sin(Mathf.PI / 3.0f * (i + 0.5f)), 0.0f, Mathf.Cos(Mathf.PI / 3.0f * (i + 0.5f))));
+			vertices.Add(new Vector3(Mathf.Sin(Mathf.PI / 3.0f * (i + 0.5f)), -6.0f, Mathf.Cos(Mathf.PI / 3.0f * (i + 0.5f))));
+			vertices.Add(new Vector3(Mathf.Sin(Mathf.PI / 3.0f * (i + 1.5f)), 0.0f, Mathf.Cos(Mathf.PI / 3.0f * (i + 1.5f))));
+			vertices.Add(new Vector3(Mathf.Sin(Mathf.PI / 3.0f * (i + 1.5f)), -6.0f, Mathf.Cos(Mathf.PI / 3.0f * (i + 1.5f))));
+			Vector3 norm = new Vector3 (Mathf.Sin (Mathf.PI / 3.0f * (i + 1.0f)), 0.0f, Mathf.Cos (Mathf.PI / 3.0f * (i + 1.0f)));
+			for (int j = 0; j < 4; j++) {
+				normals.Add(new Vector3(norm.x, norm.y, norm.z));
+			}
+			triangles.Add (sideIdx + 1);
+			triangles.Add (sideIdx + 2);
+			triangles.Add (sideIdx);
+			triangles.Add (sideIdx + 3);
+			triangles.Add (sideIdx + 2);
+			triangles.Add (sideIdx + 1);
+			sideIdx += 4;
+		}
+			
+		mesh.SetVertices (vertices);
+		mesh.SetNormals (normals);
+		mesh.SetTriangles (triangles, 0);
+
+		gameObject.GetComponent<MeshRenderer> ().material = meshMat;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/HexLibrary.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexLibrary : MonoBehaviour {
+
+	public GameObject hex;
+	private GameObject[,] gridMap = null; // the map in hex coordinates
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Borf");
+		gridMap = new GameObject[7, 7];
+		GenerateGrid (7, 7, -3, -3);
+
+	}
+
+	// creates a hexagonal grid with specified number of rows and columns (to be changed later)
+	void GenerateGrid(int rows, int cols, int minRow, int minCol) {
+		if (hex == null)
+			return;
+
+		float length = 1.0f;
+		float height = 2.0f * length;
+		float width = Mathf.Sqrt (3.0f) * length;
+
+
+		for (int col = minCol; col < minCol + cols; col++) {
+			for (int row = minRow; row < minRow + rows; row++) {
+				Vector3 centroid = new Vector3 (
+					1.0f + 1.5f * col, 
+					Random.Range(0.0f, 1.0f), 
+					width * row + ((col & 1) == 0 ? width * 0.5f : 0.0f)
+				);
+				GameObject nextHex = (GameObject) Instantiate(hex, centroid, new Quaternion());
+				Vector2 axialIndex = OffsetToAxial(new Vector2(row, col));
+				Debug.Log (axialIndex.x + ", " + axialIndex.y); 
+				gridMap [(int) axialIndex.x - minRow, (int) axialIndex.y - minCol] = nextHex;
+			}
+		}
+
+	}
+
+	/*
+	 * Input: Vector3 in worldspace
+	 * 
+	 * Output: Offset Grid index of a hex that could be here
+	 *
+	 */
+	Vector2 WorldToOffset(Vector3 wcoords) {
+		float w = 1.5f;
+		float h = Mathf.Sqrt (3) * 0.5f;
+		// transform coordinates to grid of w, h rects
+		float x = Mathf.Floor (wcoords.x / w);
+		float y = Mathf.Floor (wcoords.z / h);
+		// normalized remainder
+		float u = (wcoords.x - x * w) / w;
+		float v = (wcoords.z - y * h) / h;
+
+		if (u > 1.0f / 3.0f) {
+			// definitely inside one hexagon
+			return new Vector2 (x, (((int)y & 1) == 0 ? y : y - 1));
+		} else {
+			// two possible hexagons
+			if (((int)y & 1) == 1) {
+				if (v > (3.0f * u)) {
+					return new Vector2(x - 1, (y + 1) / 2);
+				} else {
+					return new Vector2 (x, (y - 1) / 2);
+				}
+			} else {
+				if (v < (1.0f - 3.0f * u)) {
+					return new Vector2 (x - 1, (y - 2) / 2);
+				} else {
+					return new Vector2 (x, y / 2);
+				}
+			}
+		}
+
+	}
+
+	Vector2 OffsetToAxial (Vector2 offset)
+	{
+		int x = (int)offset.x;
+		int y = (int)offset.y;
+		y = y - (x + y & 1) / 2;
+		return new Vector2(x, y);
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
